84e8a89345fed0fc59bc3394a74106d019bbb38a
unknown <castellano17@gmail.com>
2023-05-02 14:49:30 -0600

    feat(multimedia): Add Multer for multimedia management

--- src/posts/posts.router.js
+++ src/posts/posts.router.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 
 const postServices = require("./posts.services");
-const passportJwt = require("../middlewares/passport.middleware");
 const commentsServices = require("../comments/comments.services");
+const likesServices = require("../likes/likes.services");
+
+const passportJwt = require("../middlewares/passport.middleware");
 
 router
   .route("/")
@@ -24,13 +26,27 @@ router
     postServices.deletePost
   );
 
+router
+  .route("/:id/comments")
+  .get(commentsServices.getAllCommentsByPost)
+  .post(
+    passportJwt.authenticate("jwt", { session: false }),
+    commentsServices.postComment
+  );
+
+router
+  .route("/:id/likes")
+  .get(likesServices.getAllLikesByPost)
+  .post(
+    passportJwt.authenticate("jwt", { session: false }),
+    likesServices.postLike
+  );
+
 router.get(
   "/user/me",
   passportJwt.authenticate("jwt", { session: false }),
   postServices.getPostsByMyUser
 );
 router.get("/user/:id", postServices.getPostsByUser);
-
-router.route("/:id/comments").get(commentsServices.getAllCommentsByPost);
 
 module.exports = router;
