06f284e14aaf165e48fe3d19f2edb936c12ebe31
castellano17 <castellano17@gmail.com>
2023-05-04 20:00:26 -0600

    feat(documentation) add: swaggers doc

--- src/app.js
+++ src/app.js
@@ -2,18 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 require("dotenv").config();
+const swaggerUl = require("swagger-ui-express");
 
 const userRouter = require("./users/users.router");
 const authRouter = require("./auth/auth.router");
 const postRouter = require("./posts/posts.router");
 const followRouter = require("./follows/follows.router");
+const swaggerDoc = require("../swagger.json");
 
 const upload = require("./middlewares/multer.middleware");
 const db = require("./utils/database");
 const initModels = require("./models/initModels");
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 //? Validar la conexión
 
@@ -47,15 +49,14 @@ app.get("/", (req, res) => {
 
 app.get("/api/v1/uploads/:fileName", (req, res) => {
   //const fileName = req.params.fileName
-  res
-    .status(200)
-    .sendFile(path.resolve("./public/") + "/" + req.params.fileName);
+  res.status(200).sendFile(path.resolve("public/") + "/" + req.params.fileName);
 });
 
 app.post("/api/v1/add-file", upload.single("my-image"), (req, res) => {
   res.status(200).json({ message: req.file });
 });
 
+app.use("/swagger", swaggerUl.serve, swaggerUl.setup(swaggerDoc));
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/posts", postRouter);
